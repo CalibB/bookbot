40cf56080d19de19c0f2c2df2107df3f11b30f5e
CalibB <110089911+CalibB@users.noreply.github.com>
2025-02-21 09:18:57 +0200

    feat: created count_words func

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 def main():
-    # Read content from file
     with open("./books/frakenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        # print(file_contents)
+    
+    print(count_words(file_contents))
+
+def count_words(text):
+    words = text.split()
+    count = 0
+    for word in words:
+        count += 1
+    
+    return count
 
 main()
