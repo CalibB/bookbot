040169b5af575af7402bceef2d4051abff139a39
CalibB <110089911+CalibB@users.noreply.github.com>
2025-02-21 09:34:28 +0200

    feat: created count_characters func

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main():
         file_contents = f.read()
         # print(file_contents)
     
-    print(count_words(file_contents))
+    # print(count_words(file_contents))
+    print(count_characters(file_contents))
+    
 
 def count_words(text):
     words = text.split()
@@ -12,5 +14,16 @@ def count_words(text):
         count += 1
     
     return count
+
+def count_characters(text):
+    sanitized_text = text.lower()
+    char_count = {}
+    for char in sanitized_text:
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+    
+    return char_count
 
 main()
